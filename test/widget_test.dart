d8e6a6f9f1cc236a52360fa15bc37953bbbe0a74
Tsukasa <belle.sobakasu@gmail.com>
2024-06-16 15:17:31 +0900

    フォルダ

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sake_wine/main.dart';
+import 'package:sake_wine/presentation/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
